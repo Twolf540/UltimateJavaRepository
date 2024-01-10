967622947c8ad62aee6c97e0ff6c0ea83a938a94
Twolf540 <tyler.carey06@gmail.com>
2024-01-10 09:39:40 -0600

    finished 1_2 notes

--- Unit01/Topic1_2/Main.java
+++ Unit01/Topic1_2/Main.java
@@ -2,7 +2,10 @@ package Unit01.Topic1_2;
 
 public class Main {
   public static void main(String[] args) {
-    // Your code goes here
+int marblesInJar;
+double testAverage;
+boolean hasSugar;
+final int carTires;
   }
   
 }
