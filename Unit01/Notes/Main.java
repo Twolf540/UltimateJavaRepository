f3512d7ec2337eddbaabf8341238be1bd22f0cdf
Twolf540 <tyler.carey06@gmail.com>
2024-01-11 10:24:37 -0600

    some notes

---
+++ Unit01/Notes/Main.java
@@ -0,0 +1,10 @@
+package Unit01.Notes;
+
+public class Main {
+    public static void main(String[] args) {
+        int grade = ((int)(Math.random()*4) + 9);
+        System.out.println(grade);
+    }
+
+
+}
--- Unit01/Topic1_1/Notes/Main.java
+++
@@ -1,7 +0,0 @@
-package Unit01.Topic1_1.Notes;
-
-public class Main {
-    int grade = ((int)(Math.random()*4) + 9);
-    System.out.println(grade);
-
-}
