1fe068e8e19b58c89f09e1651a9821bbf549f5fa
Twolf540 <tyler.carey06@gmail.com>
2024-01-14 14:21:58 -0600

    wrote TwoDSix Program

--- Unit01/Xtras/TwoDSix.java
+++ Unit01/Xtras/TwoDSix.java
@@ -2,6 +2,9 @@ package Unit01.Xtras;
 
 public class TwoDSix {
     public static void main(String[] args) {
-
+        int result1 = (int) (Math.random() * (6)) + 1;
+        int result2 = (int) (Math.random() * (6)) + 1;
+        int resultSum = (int) (result1 + result2);
+        System.out.println(resultSum);
     }
 }
