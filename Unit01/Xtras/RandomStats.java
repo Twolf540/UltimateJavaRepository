e433a1c2ea5a6c0aa7a1b47d2f93780c7d6d5925
Twolf540 <tyler.carey06@gmail.com>
2024-01-15 14:31:54 -0600

    wrote RandomStats program

--- Unit01/Xtras/RandomStats.java
+++ Unit01/Xtras/RandomStats.java
@@ -23,15 +23,15 @@ public class RandomStats {
         //finds the lowest value for value(1-5)
         double minimumEnd = Math.min(minimum6,minimum8);
         //finds the highest values between the five doubles value(1-5)
-        double maximum1 = Math.min(value1, value2);
-        double maximum2 = Math.min(value2, value3);
-        double maximum3 = Math.min(value3, value4);
-        double maximum4 = Math.min(value4, value5);
-        double maximum5 = Math.min(value5, value1);
+        double maximum1 = Math.max(value1, value2);
+        double maximum2 = Math.max(value2, value3);
+        double maximum3 = Math.max(value3, value4);
+        double maximum4 = Math.max(value4, value5);
+        double maximum5 = Math.max(value5, value1);
         //lowers the possible highestvalues values down to two
-        double maximum6 = Math.min(maximum1, maximum2);
-        double maximum7 = Math.min(maximum3, maximum4);
-        double maximum8 = Math.min(maximum7, maximum5);
+        double maximum6 = Math.max(maximum1, maximum2);
+        double maximum7 = Math.max(maximum3, maximum4);
+        double maximum8 = Math.max(maximum7, maximum5);
         //finds the highest value for value(1-5)
         double maximumEnd = Math.max(maximum6,maximum8);
         //Prints the five values, their average, and the maximum and minimum values in the set
