f38d6f65524039eab318ce6e790c32291d445fc3
Twolf540 <tyler.carey06@gmail.com>
2024-01-18 10:24:56 -0600

    2_2 Notes

---
+++ Unit02/Topic2_2/School.java
@@ -0,0 +1,8 @@
+package Unit02.Topic2_2;
+
+public class School {
+    public static void main(String[] args) {
+        Student s1 = new Student(17, "Abby");
+    }
+    
+}
---
+++ Unit02/Topic2_2/Student.java
@@ -0,0 +1,12 @@
+package Unit02.Topic2_2;
+
+public class Student {
+    int age;
+    String name;
+
+    public Student(int a, String n) {
+        age = a;
+        name = n;
+    }
+
+}
