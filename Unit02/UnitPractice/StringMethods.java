c65e48d1cafb0ee30321e57446f0984b52263c08
Twolf540 <tyler.carey06@gmail.com>
2024-01-24 10:12:38 -0600

    string methods programs

--- Unit02/UnitPractice/StringMethods.java
+++ Unit02/UnitPractice/StringMethods.java
@@ -3,26 +3,39 @@ package Unit02.UnitPractice;
 public class StringMethods {
 
     public static String helloName(String name) {
-        return "";
+        String helloName = new String(name);
+        return ("Hello " + helloName + "!");
     }
 
     public static String firstHalf(String str) {
-        return "";
+        int stringLength = str.length();
+        int halfLength = (stringLength / 2);
+        String half1 = str.substring(0,halfLength);
+        return half1;
     }
 
     public static boolean endsLy(String str) {
-        return false;
+        int adverbLength = str.length();
+        String adverbLy = str.substring((adverbLength - 2));
+        boolean hasLy = adverbLy.equals("ly");
+        return hasLy;
     }
 
     public static String makeAbba(String a, String b) {
-        return "";
+        String stringA = new String(a);
+        String stringB = new String(b);
+        return (stringA + stringB + stringB + stringA);
     }
 
     public static String withoutEnd(String str) {
-        return "";
+        String ended = new String(str);
+        int endedLength = ended.length();
+        String endless = ended.substring(1,(endedLength-1));
+        return endless;
     }
 
     public static String nTwice(String str, int n) {
+        int stringLength = str.length();
         return "";
     }
 
