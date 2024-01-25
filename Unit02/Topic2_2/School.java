cb1dbd4977bf276f252acbbd378b8920330ae0c6
Twolf540 <tyler.carey06@gmail.com>
2024-01-25 10:21:26 -0600

    getting rid of notifications

--- Unit02/Topic2_2/School.java
+++ Unit02/Topic2_2/School.java
@@ -2,7 +2,7 @@ package Unit02.Topic2_2;
 
 public class School {
     public static void main(String[] args) {
-        Student s1 = new Student(17, "Abby");
+        // Student s1 = new Student(17, "Abby");
     }
     
 }
