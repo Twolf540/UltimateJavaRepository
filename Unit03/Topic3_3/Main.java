089373ba6900a50461f25f7a2a48502248bfe879
Twolf540 <tyler.carey06@gmail.com>
2024-01-30 09:30:44 -0600

    3_3 Notes

--- Unit03/Topic3_3/Main.java
+++ Unit03/Topic3_3/Main.java
@@ -1,8 +1,19 @@
 package Unit03.Topic3_3;
-
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
 
+ Scanner in = new Scanner (System.in);
+    int dogAge = (Integer.parseInt(in.nextLine()));
+
+  if(dogAge < 2){
+  System.out.println("Your dog is still a puppy");
+  }
+  
+  else{
+      System.out.println("Your dog is no longer a puppy");
+  }
+  in.close();
   }
 
 }
--- Unit03/Topic3_3/README.md
+++ Unit03/Topic3_3/README.md
@@ -1,1 +1,17 @@
 # 3.3 - `if-else` Statements
+## 3_3 P1
+If-else statements allow you to do a "catch-all" for any inputs or parameters that do not fit your if statements
+```
+if(dogAge < 2){
+    System.out.println("Your dog is still a puppy");
+}
+
+else{
+    System.out.println("Your dog is no longer a puppy");
+}
+```
+
+## 3_3 P2
+Nothing new, just examples
+
+Wrote code in Main.java
