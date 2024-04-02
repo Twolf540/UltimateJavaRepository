42d037f619cc8d0004b76961ecef67b7135020a3
Twolf540 <tyler.carey06@gmail.com>
2024-04-02 10:11:28 -0500

    c

--- Unit08/Topic8_2Traversing2DArrays.java
+++ Unit08/Topic8_2Traversing2DArrays.java
@@ -2,15 +2,15 @@ package Unit08;
 
 public class Topic8_2Traversing2DArrays {
     public static void main(String[] args) {
-        public static int findFirstNonBlueColumn(Color [][] picture){
-            for (int c = 0; c < picture[0].length; c++){
-                for (int r = 0; r < picture.length; r++){
-                    if(!picture[r][c].equals(Color.BLUE)){
-                        return c;
-                    }
-                }    
-            }
-            return -1;
-        }
+        // public static int findFirstNonBlueColumn(Color [][] picture){
+        //     for (int c = 0; c < picture[0].length; c++){
+        //         for (int r = 0; r < picture.length; r++){
+        //             if(!picture[r][c].equals(Color.BLUE)){
+        //                 return c;
+        //             }
+        //         }    
+        //     }
+        //     return -1;
+        // }
     }
 }
