3968e7f0872efc9c5dec1d2f4035be7aea7796d5
Twolf540 <tyler.carey06@gmail.com>
2024-04-16 10:30:05 -0500

    c

--- ProgrammingProjects/OpticalIllusions/EisensteinIllusion.java
+++ ProgrammingProjects/OpticalIllusions/EisensteinIllusion.java
@@ -8,7 +8,18 @@ public class EisensteinIllusion extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        Color black = new Color(0,0,0);
+        Color red = new Color(230, 10, 10);
+        g.setColor(black);
+        for (int angle = 0; angle < 360; angle += 9) {
+            g.drawLine(190, 170, 
+             200 + (int) (400 * Math.cos(angle / 180.0 * Math.PI)), 
+             200 + (int) (400 * Math.sin(angle / 180.0 * Math.PI)));
 
+        }
+        g.setColor(red);
+        g.drawRect(240, 110, 125, 125);
+        g.drawOval(25, 110, 125, 125);
     }
 
     public static void main(String[] args) {
