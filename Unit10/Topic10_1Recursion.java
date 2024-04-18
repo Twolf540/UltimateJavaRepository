12edcd1c7b83d8a81b6a60ea131267ff74e94d8b
Twolf540 <tyler.carey06@gmail.com>
2024-04-18 10:30:06 -0500

    c

--- Unit10/Topic10_1Recursion.java
+++ Unit10/Topic10_1Recursion.java
@@ -1,7 +1,20 @@
 package Unit10;
 
 public class Topic10_1Recursion {
-    public static void main(String[] args) {
 
+    public static int sumOfArray(int[] a, int position) {
+        // write a recursive function that returns the sum of the elements
+        if(position == a.length){
+            return 0;
+        }
+        int curr = a[position];
+        if(position < a.length){
+            return curr + sumOfArray(a, position + 1);
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOfArray(new int[] {3, 1, 5, 2}, 0 ));
     }
 }
